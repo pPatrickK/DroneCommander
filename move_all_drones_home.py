c499deed5aa96cee6797acf286cece8caa0e7f39
flw <patrick.klokowski@tu-dortmund.de>
2018-07-04 18:03:25 +0200

    DroneComamnder move all drones home is implemented but not testes yet

--- move_all_drones_home.py
+++ move_all_drones_home.py
@@ -51,22 +51,51 @@ class Line(object):
         distance_vector = self.start - self.end
         return distance_vector.length()
 
+def get_keys(dictionary):
+    return dictionary.keys()
+
 def get_potential_colliding_drones(drones):
     result = []
-    number_of_drones = len(drones)
+    drone_keys = get_keys(drones)
+    number_of_drones = len(drone_keys)
     for i in range(number_of_drones - 1):
-        drone_1 = drones[i]
+        key_1 = drone_keys[i]
+        drone_1 = drones[key_1]
         drone_1_movement_line = get_home_line(drone_1)
         for j in range(i + 1, number_of_drones):
-            drone_2 = drones[j]
+            key_2 = drone_keys[j]
+            drone_2 = drones[key_2]
             drone_2_movement_line = get_home_line(drone_2)
             if drone_1_movement_line.doCollide(drone_2_movement_line):
-                result.append((drone_1, drone_2))
+                result.append((key_1, key_2))
     return result
 
 def get_non_colliding_drones(drones, potential_colliding_drones):
     drones = copy.copy(drones)
     for (d0, d1) in potential_colliding_drones:
-        drones.remove(d0)
-        drones.remove(d1)
-    return drones
+        drones.pop(d0)
+        drones.pop(d1)
+    return get_keys(drones)
+
+def make_move_home_list(drones, land_height, droneCommander=None):
+    time_delta = 0.1
+    time_landing = 3.0
+    commands = []
+    time_offset = 0.0
+    if droneCommander != None:
+        time_offset = droneCommander.start_time
+    drones = copy.copy(drones)
+    while len(drones) > 0:
+        colliding_drones = get_potential_colliding_drones(drones)
+        non_colliding_drones = get_non_colliding_drones(drones, colliding_drones)
+        if len(non_colliding_drones) == 0: # of no potential candidates, take one random
+            non_colliding_drones = [drones[0]]
+        print non_colliding_drones
+        for id in non_colliding_drones:
+            time = get_home_line(drone).length() # m/s
+            commands.append((id, make_command(id, DroneMoveHomeCommand, offset, time)))
+            offset += time + time_delta
+            commands.append((id, make_command(id, DroneLandWithHeightCommand, offset, time_landing, land_height)))
+            offset += time_landing + time_delta
+            drones.pop(id)
+    return commands
