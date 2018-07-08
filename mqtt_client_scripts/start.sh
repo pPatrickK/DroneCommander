2dafbe4c44ec48a1b90e73929325f52505d8df5a
Patrick <patrick.klokowski@tu-dortmund.de>
2018-07-08 18:18:37 +0200

    added script to start all drones and modifed original start script

--- mqtt_client_scripts/start.sh
+++ mqtt_client_scripts/start.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 echo Hello start
 mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 1.5, 1.0]}' -t crazyflie/start
+
---
+++ mqtt_client_scripts/start_all.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo Hello start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":1, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":2, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":3, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":4, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":5, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":6, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":7, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":8, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":9, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":10, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":11, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":12, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":13, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":14, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":15, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":16, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":17, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":18, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":19, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":20, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":21, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":22, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":23, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":24, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":25, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":26, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":29, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+mosquitto_pub -h gopher.phynetlab.com -p 8883 -m '{"id":28, "data":[0.0, 2.5, 1.0]}' -t crazyflie/start
+
